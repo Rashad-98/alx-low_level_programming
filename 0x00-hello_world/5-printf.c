45db7ce8fbb806eb69d9319531280ab351d67af3
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-03-16 09:05:05 -0700

    use printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the output is a piece of art,");
+
+	return (0);
+}
