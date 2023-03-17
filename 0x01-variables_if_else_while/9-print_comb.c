5d7a6aa971c9fc8581fd00db36971159a073bb56
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-03-17 11:32:47 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i != 57)
+			putchar(',');
+		i++;
+	}
+	putchar('$');
+	putchar('\n');
+
+	return (0);
+}
