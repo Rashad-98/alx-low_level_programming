35b6308fa702fb362dfb343b306819d28e89d492
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-03-21 11:08:17 -0700

    fix alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,19 @@
 #include "main.h"
 
+/**
+ * print_alphabet - print the alphabets in lowercase according to ascii
+ *
+ * Return: void
+ */
 void print_alphabet(void)
 {
 	int i = 97;
-	while(i <= 122)
+
+	while (i <= 122)
 	{
 		_putchar(i);
 		i++;
 	}
+	putchar('\n');
 }
+
