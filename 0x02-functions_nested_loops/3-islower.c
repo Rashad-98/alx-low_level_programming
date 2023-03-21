17e296f87fe92f3969c523c21f29a7441f3841f2
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-03-21 11:41:39 -0700

    check is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lower case letters
+ *@c: the character to pirnt
+ *
+ * Return: 1 if lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 && !(c > 122))
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
