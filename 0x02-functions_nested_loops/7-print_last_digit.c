d3fd87b19a436a7485a8fa18cc2e9eef605dc0f4
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-03-21 17:41:56 -0700

    fix printing last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,16 +5,15 @@
  * print_last_digit - prints the last digit of a number
  *@n: the number which its last digit to be printed
  *
- * Retrun: the last digit of a number as integer
+ * Return: the last digit of a number as integer
  */
 int print_last_digit(int n)
 {
 	if (n < 0)
 	{
 		int abs_n = n * (-1);
-		/*int x = n << 1;
-		int abs_n = x >> 1;*/
 		int last_digit = abs_n % 10;
+
 		if (n == INT_MIN)
 			last_digit = 8;
 		_putchar('0' + last_digit);
