658f262f823efff44fabe686e1e2059d0142549a
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-03-27 07:49:01 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - puts char to stdout with a newline
+ * @str: pointer ot string
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
