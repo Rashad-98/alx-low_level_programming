bb9234779878d5b359fc40e9b7cd7611a854ae2f
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-03-27 07:04:47 -0700

    update directory name

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes pointer to integer and updates the value of the integer.
+ * @n: pointer to integer.
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
