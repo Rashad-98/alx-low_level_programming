8eaf9ed291831265de2fe7f586fc185d8e376b8c
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-03-27 08:15:39 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string iin revers
+ * @s: string to be reversed
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+	int length;
+
+	i = 0;
+	length = 0;
+	while (*(s + i) != '\0')
+	{
+		length++;
+		i++;
+	}
+
+	char revstr[length];
+	int x;
+
+	x = 0;
+	while (x < length)
+	{
+		revstr[x] = *(s + length - 1);
+		x++;
+	}
+
+	return (revstr);
+}
