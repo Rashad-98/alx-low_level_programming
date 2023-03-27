f39a0b3ad518a6b45733f62201fe29c77d54417c
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-03-27 08:44:18 -0700

    print every character in string followed by new line

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; *(str + i) != '\0'; i++)
+	for (i = 0; *(str + i) != '\0'; i+=2)
 	{
 		_putchar(*(str + i));
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
