cfb98e347f6554b18d5b7caee6e0d48a8e2ff5f4
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-03-29 20:26:36 -0700

    concatenate strings based on n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination strings
+ * @src: source string
+ *
+ * Return: pointer to destination string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int y;
+	int dest_length;
+
+	i = 0;
+	y = 0;
+	dest_length = 0;
+	while (1)
+	{
+		if (*(dest + i) == '\0')
+		{
+			while ((*(src + y) != '\0') || (y + 1 == n))
+			{
+				*(dest + dest_length + y) = *(src + y);
+				y++;
+			}
+			return (dest);
+		}
+		dest_length++;
+		i++;
+	}
+}
