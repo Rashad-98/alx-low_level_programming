84e98feeffe633bc158e9887e2d6a5442467e29b
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-04-05 04:56:39 -0700

    print using recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string to screen using recursion
+ * @s: pointer to string to be printed
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\n')
+		return;
+	_putchar(*s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+this is the first recurion task at alx.
