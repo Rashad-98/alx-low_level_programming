7ce01b32167086cde559a848edd5dcfc88de524c
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-04-10 04:58:36 -0700

    multiply from command line

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * main - program to multiply two arguments
+ * @argc: arguments count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 when no arguments are passed
+ */
+int main(int argc, char **argv)
+{
+	int num1;
+	int num2;
+	int result;
+	char str[100];
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(*(argv + 1));
+	num2 = atoi(*(argv + 2));
+	result  = num1 * num2;
+	sprintf(str, "%d", result);
+	printf("%s\n", str);
+
+	return (0);
+}
