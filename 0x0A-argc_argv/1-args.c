469a479f91d152f378b82aff8f0c54c805a4972a
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-04-10 04:05:33 -0700

    add two parameters to main, not one

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,10 +3,11 @@
 /**
  * main - program that prints the number of arguments passed to it
  * @argc: arguments count
+ * @argv: argument vector
  *
  * Return: 0 always
  */
-int main(int argc)
+int main(int argc, char **argv)
 {
 	printf("%d\n", argc - 1);
 
