5070079c26794e4eb17914219f2b26ce5cfa4467
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-04-10 05:40:01 -0700

    add numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - program that prints the result of addition of numbers entered as CL arguments
+ * @argc: arguments count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 if a character that is not a number is entered
+ */
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		printf("0\n");
+	else
+	{
+		int o;
+		int i;
+		int sum;
+
+		sum = 0;
+		for (o = 1; o < argc; o++)
+		{
+			for (i = 0; i != '\0'; i++)
+			{
+				if (!isdigit(**(argv + i)))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(*(argv + o));
+		}
+
+		printf("%d\n", sum);
+	}
+
+	return (0);
+}
