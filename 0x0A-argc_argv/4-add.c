98b8902b6f00f137984b5d63bc48523896b314ae
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-04-10 06:06:54 -0700

    fix pointer arithmetic

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,7 +3,8 @@
 #include <ctype.h>
 
 /**
- * main - program that prints the result of addition of numbers entered as CL arguments
+ * main - program that prints the result of
+ * addition of numbers entered as CL arguments
  * @argc: arguments count
  * @argv: argument vector
  *
@@ -22,9 +23,9 @@ int main(int argc, char **argv)
 		sum = 0;
 		for (o = 1; o < argc; o++)
 		{
-			for (i = 0; **(argv + i) != '\0'; i++)
+			for (i = 0; *(*(argv + o) + i) != '\0'; i++)
 			{
-				if (!isdigit(**(argv + i)))
+				if (!isdigit(*(*(argv + o) + i)))
 				{
 					printf("Error\n");
 					return (1);
