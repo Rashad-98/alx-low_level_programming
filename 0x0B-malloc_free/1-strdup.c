bba144aa8656907533e42ef14755538e443956d1
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-04-11 15:43:37 -0700

    copy string to a dynamically allocated memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - copies string and returns pointer to
+ * the dynamically allocated copy
+ * @str: pointer to string to copy
+ *
+ * Return: pointer to copied string
+ */
+char *_strdup(char *str)
+{
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		char *strcp;
+		int i;
+		int length;
+
+		for (i = 0; *(str + i) != '\0'; i++)
+		{
+			length++;
+		}
+
+		strcp = malloc((sizeof(char) * length) + 1);
+
+		if (strcp == NULL)
+			return (NULL);
+
+		for (i = 0; *(str + i) != '\0'; i++)
+		{
+			*(strcp + i) = *(str + i);
+		}
+		*(strcp + length) = '\0';
+
+		return (strcp);
+	}
+}
