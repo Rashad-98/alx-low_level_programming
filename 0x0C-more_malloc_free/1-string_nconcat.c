bd7304f3f3a17962c859d89133d4759acda08605
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-04-13 07:14:44 -0700

    fix types for comparison

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,9 +10,9 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i;
-	int s1length;
-	int s2length;
+	unsigned int i;
+	unsigned int s1length;
+	unsigned int s2length;
 	char *ns;
 
 	s1length = 0;
