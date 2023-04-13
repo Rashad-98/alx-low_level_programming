00ea8fbe2ada4b5e8ec0559123f82ef843dbafd4
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-04-13 07:56:37 -0700

    write calloc function

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - same as calloc. (man calloc)
+ * @nmemb: number of elements in array
+ * @size: size of each element
+ *
+ * Return: pointer to new array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		char *ptr;
+		unsigned int i;
+		
+		ptr = malloc(nmemb * size);
+		if (ptr == NULL)
+			return (NULL);
+		
+		for (i = 0; i < (nmemb * size); i++)
+			*(ptr + i) = '\0';
+
+		return (ptr);
+	}
+}
