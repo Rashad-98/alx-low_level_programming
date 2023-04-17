3bf829261fbae0639fe8d207dd5407393d986bf1
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-04-17 11:01:43 -0700

    make a function to create a varible of type dog_t

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - create a varible of type dot_t
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to dot_t
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d = malloc(sizeof(dog_t));
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+	return (d);
+}
