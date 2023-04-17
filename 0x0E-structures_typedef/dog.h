054d0281d7e213e634eb85c0fa4b9d04e98a9651
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-04-17 09:17:38 -0700

    create a structure to store dog information

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - a record for dogs
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
