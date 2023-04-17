b71cad158caa2ccb3c035c2cf27c1d2e1bed66ff
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-04-17 11:05:14 -0700

    fix for betty coding style

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,6 +13,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d = malloc(sizeof(dog_t));
 
+	if (d == NULL)
+		return (NULL);
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
