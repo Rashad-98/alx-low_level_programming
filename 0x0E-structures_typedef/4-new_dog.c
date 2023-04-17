dc5a0dd2ae7e5cbd441ce9635fd9107752b8cc04
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-04-17 11:32:24 -0700

    do not use stlen

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * new_dog - create a varible of type dot_t
@@ -12,10 +11,21 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	unsigned int long i;
-	char *the_name = malloc(strlen(name) + 1);
-	char *the_owner = malloc(strlen(owner) + 1);
+	unsigned int long i, n_length, o_length;
+	char *the_name;
+	char *the_owner;
 	dog_t *d = malloc(sizeof(dog_t));
+
+	n_length = 0;
+	for (i = 0; *(name + i) != '\0'; i++)
+		n_length++;
+
+	o_length = 0;
+	for (i = 0; *(owner + i) != '\0'; i++)
+		o_length++;
+
+	the_name = malloc((n_length + 1) * sizeof(char));
+	the_owner = malloc((o_length + 1) * sizeof(char));
 
 	if (the_name == NULL)
 		return (NULL);
