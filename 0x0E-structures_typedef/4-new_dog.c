cca4d5b97275183c9452db92816c27b29021ccf7
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-04-17 11:16:55 -0700

    use strlen

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - create a varible of type dot_t
@@ -11,14 +12,25 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	char *the_name = malloc(strlen(name) + 1);
+	char *the_owner = malloc(strlen(owner) + 1);
 	dog_t *d = malloc(sizeof(dog_t));
+
+	if (the_name == NULL)
+		return NULL;
+
+	if (the_owner == NULL)
+		return NULL;
 
 	if (d == NULL)
 		return (NULL);
 
-	d->name = name;
+	strcpy(the_name, name);
+	strcpy(the_owner, owner);
+
+	d->name = the_name;
 	d->age = age;
-	d->owner = owner;
+	d->owner = the_owner;
 
 	return (d);
 }
