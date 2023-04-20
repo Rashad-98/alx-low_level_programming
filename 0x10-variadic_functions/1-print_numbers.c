2a696c1ea233aac94486b7868f66987d802af2a7
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-04-20 13:02:42 -0700

    fix for null separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,6 +15,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
+		if (!separator)
+			continue;
 		if (i < (n - 1))
 			printf("%s", separator);
 	}
