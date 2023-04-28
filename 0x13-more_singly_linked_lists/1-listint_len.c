986a142a38d102f140b2a28e5fd585f750141ca0
Rashad98 <a.rashaad98@gmail.com>
2023-04-28 19:10:19 +0200

    print number of list nodes

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - get the number of nodes in 
+ * singly linked list
+ * @h: pointer to head of list
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	if (!(h == NULL))
+	{
+		return (listint_len(h->next) + 1);
+	}
+
+	return (0);
+}
