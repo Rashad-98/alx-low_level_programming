b1744f2205abde9d38fd980814160d86eced9f82
Rashad98 <a.rashaad98@gmail.com>
2023-04-29 14:50:45 +0200

    push to singly linked list

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - add a node to the end of singly linked list
+ * @head: pointer to pointer to head of list
+ * @n: value of new node
+ *
+ * Return: new node
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	if (*head == NULL)
+	{
+		listint_t *new;
+
+		new = malloc(sizeof(listint_t));
+
+		if (new == NULL)
+			return (NULL);
+
+		new->n = n;
+		new->next = NULL;
+
+		(*head) = new;
+
+		return (new);
+	}
+	else
+	{
+		if ((*head)->next == NULL)
+		{
+			listint_t *new = malloc(sizeof(listint_t));
+
+			if (new == NULL)
+				return (NULL);
+
+			new->n = n;
+			new->next = NULL;
+
+			(*head)->next = new;
+
+			return (new);
+		}
+	}
+
+		add_nodeint_end(&((*head)->next), n);
+
+		return (0);
+}
