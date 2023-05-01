a56045563db2d6dab5d5662ac04fce8e963f5c79
Rashad98 <a.rashaad98@gmail.com>
2023-05-01 15:22:33 +0200

    fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -21,10 +21,17 @@ void free_listint(listint_t *head)
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *tmp;
+
 	if (head == NULL || *head == NULL)
 		return;
 
-	free_listint(*head);
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
 
 	*head = NULL;
 }
