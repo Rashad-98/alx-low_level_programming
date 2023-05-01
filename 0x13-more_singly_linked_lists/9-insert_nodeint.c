d84245dbbb4dfa41d523eddcea3eb3848ccf3c8e
Rashad98 <a.rashaad98@gmail.com>
2023-05-01 16:58:11 +0200

    insert into singly linked list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts node in a singly linked list at a
+ * given index
+ * @head: pointer to pointer to list's head
+ * @idx: index
+ * @n: value of new node
+ *
+ * Return: address of new node, of NULL on failure
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *tmp = *head;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < idx; i++)
+	{
+		if (tmp == NULL)
+		{
+			return (NULL);
+		}
+		if ((i + 1) == idx)
+		{
+			listint_t *new, *tmp2;
+
+			new = malloc(sizeof(listint_t));
+			if (new == NULL)
+				return (NULL);
+
+			new->n = n;
+
+			tmp2 = tmp->next;
+			tmp->next = new;
+			new->next = tmp2;
+		}
+		tmp = tmp->next;
+	}
+
+	return (NULL);
+}
