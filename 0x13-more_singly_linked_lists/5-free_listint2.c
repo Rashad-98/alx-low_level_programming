baf894993f1b73a0cc7e9dec02f59ed8cb79282f
Rashad98 <a.rashaad98@gmail.com>
2023-05-01 13:31:46 +0200

    free singly linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a singly linked list of integers and sets
+ * a pointer to a pointer to its head to NULL.
+ * @head: pointer to pointer to list's head
+ */
+void free_listint2(listint_t **head)
+{
+	free_listint(*head);
+
+	head == NULL;
+}
