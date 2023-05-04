d23bbf7e811ad4858b5e6c6feb9a0d369c713980
Rashad98 <a.rashaad98@gmail.com>
2023-05-04 22:15:45 +0300

    setup for print_binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary representation of number
+ * @n: number
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int i;
+
+	for (i = 0; i <= ULONG_MAX; i++)
+	{
+
+	}
+}
