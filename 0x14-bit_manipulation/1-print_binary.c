b39c9ee5e26e38d656bde190c3f13f459838294a
Rashad-98 <a.rashaad98@gmail.com>
2023-05-04 23:58:46 +0300

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,10 +6,32 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int i;
+	unsigned long int i, ncpy, bits;
 
-	for (i = 0; i <= ULONG_MAX; i++)
+	if (n == 0)
 	{
+		_putchar('0');
+		return;
+	}
 
+	bits = 0;
+	ncpy = n;
+	for (i = 0; i < 20; i++)
+	{
+		if (ncpy == 1)
+		{
+			_putchar('1');
+			bits = i + 1;
+			break;
+		}
+		ncpy >>= 1;
+	}
+
+	ncpy = n;
+	for (i = 2; i <= bits; i++)
+	{
+		ncpy >>= bits - i;
+		_putchar((ncpy & 1) + '0');
+		ncpy = n;
 	}
 }
---
+++ 0x14-bit_manipulation/check.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int x = 'A' + '0';
+
+	printf("%d\n", x);
+
+	return (0);
+}
