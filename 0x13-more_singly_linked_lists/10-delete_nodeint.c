8a398058753eac986db922e903ecdff0be3d5cee
Rashad98 <a.rashaad98@gmail.com>
2023-07-06 00:59:12 +0300

    initial setup for removing node at a given index

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * delete_nodeint_at_index - deletes node from list at given index
+ * @head: pointer to head of the list
+ * @index: index of node to be deleted
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	printf("hello world!");
+	return (1);
+}
