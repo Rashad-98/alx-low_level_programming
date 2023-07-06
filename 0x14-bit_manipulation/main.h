e5370eaff9b73708a920e6e5c0433b4051c47302
Rashad98 <a.rashaad98@gmail.com>
2023-07-06 16:37:13 +0300

    add clear_bit to header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,4 +6,5 @@ void print_binary(unsigned long int n);
 void _putchar(char);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int cleaer_bit(unsigned long int *n, unsigned int index);
 #endif
